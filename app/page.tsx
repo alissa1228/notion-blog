70ac9a078e8ee6f95642742d6696947fedd55a01
mac1228 <68986081+alissa1228@users.noreply.github.com>
2025-03-30 22:46:39 +0900

    #7 자기소개 맨 앞에 간략하게 변경(from. 바이브 코딩)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import { BlogPosts } from 'components/posts'
+import Image from "next/image";
+import { BlogPosts } from "components/posts";
 
 export default function Page() {
   return (
     <section>
-      <div className="flex items-center mb-8">
+      <div className="flex flex-col items-center space-y-6 mb-8">
         <Image
           src="/new-profile.webp"
           alt="profile image"
@@ -12,20 +12,23 @@ export default function Page() {
           height={100}
           className="rounded-full mr-4 border border-gray-200"
         />
-        <h1 className="text-2xl font-semibold tracking-tighter">
-          Welcome to my playaground
-        </h1>
+        <h1 className="text-5xl font-bold tracking-tight">👋 JUNGMIN YOON</h1>
+        <div className="text-muted-foreground text-center">
+          <p className="text-lg font-semibold mb-4">
+            Frontend Developer → Project Manager✈️
+          </p>
+          <p>
+            아이디어를 현실로 만드는데 진심인 사람입니다.
+            <br />
+            평소에 보고, 읽고, 겪고, 느낀 것들을 기록하고 있습니다.
+          </p>
+        </div>
       </div>
-      <p className="mb-4">
-        {`안녕하세요.👋 2년 차 프론트엔드 개발자, 서비스 기획자 직무 이동을 준비 중인 윤정민입니다. 아이디어를 현실로 만드는 사람이 되고자 합니다. 팀 효율성 증진에 관심이 많습니다. 평소에 보고, 읽고, 겪은 것들을 기록하고 있습니다. `}
-      </p>
-      {/* <p className="mb-4">
-        {`Hello!👋 I’m Jungmin Yoon, a frontend developer with two years of experience. I’m passionate about building content-centered products and improving team efficiency. I regularly document insights from what I see, read, and learn to enhance my skills and knowledge.`}
-      </p> */}
-      <hr className='mb-4'/>
+
+      <hr className="mb-4" />
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
