62e6a3d841ec2d857f510b60e4badf6ab3466978
mac1228 <68986081+alissa1228@users.noreply.github.com>
2025-03-31 02:08:32 +0900

    #9 개발자-> 기획자 레쥬메로 변경

--- content/resume.ts
+++ content/resume.ts
@@ -1,4 +1,4 @@
-import resumeContent from '../notion-data/20263ff7-6374-449d-b66f-7fd37fafa36b.json'
+import resumeContent from '../notion-data/1c633ccb-0cd0-8014-bda7-edc15cf824ca.json'
 
 export const resume = {
     title: "Resume",
