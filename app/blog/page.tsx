abdf2b6c798a5f48e42395968c3a48bb1d23dc67
mac1228 <68986081+alissa1228@users.noreply.github.com>
2025-04-03 00:51:26 +0900

    #10 blog  네오브루탈리즘 디자인 완

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,7 +8,10 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <div className='mb-8 tracking-tighter px-8 py-16 border-b-2 border-black'>
+      <h1 className="font-semibold text-2xl">My Blog</h1>
+      <p>블로그용 글 아카이브</p>
+      </div>
       <BlogPosts />
     </section>
   )
