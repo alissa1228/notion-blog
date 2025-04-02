903fd811f1a61513654be407a2e10c27d8d4c614
mac1228 <68986081+alissa1228@users.noreply.github.com>
2025-04-02 12:53:03 +0900

    #10 네오브루탈리즘 디자인 기반 수정 시작

--- app/page.tsx
+++ app/page.tsx
@@ -6,43 +6,42 @@ import TestPage from "./testPage";
 
 export default function Page() {
   return (
-    <TestPage/>
-    // <section>
-    //   <div className="flex flex-col items-center space-y-6 mb-8">
-    //     <Image
-    //       src="/new-profile.webp"
-    //       alt="profile image"
-    //       width={100}
-    //       height={100}
-    //       className="rounded-full mr-4 border border-gray-200"
-    //     />
-    //     <h1 className="text-5xl font-bold tracking-tight">JUNGMIN YOON 👩🏻‍💻</h1>
-    //     <div className="text-muted-foreground text-center">
-    //       <p className="text-lg font-semibold mb-4">
-    //         Frontend Developer → Project Manager✈️
-    //       </p>
-    //       <p>
-    //         아이디어를 현실로 만드는데 진심인 사람입니다.
-    //         <br />
-    //         평소에 보고, 읽고, 겪고, 느낀 것들을 기록하고 있습니다.
-    //       </p>
-    //     </div>
-    //   </div>
+    <section>
+      <div className="flex flex-col items-center space-y-6 mb-8">
+        <Image
+          src="/new-profile.webp"
+          alt="profile image"
+          width={100}
+          height={100}
+          className="rounded-full mr-4 border border-gray-200"
+        />
+        <h1 className="text-5xl font-bold tracking-tight">JUNGMIN YOON 👩🏻‍💻</h1>
+        <div className="text-muted-foreground text-center">
+          <p className="text-lg font-semibold mb-4">
+            Frontend Developer → Project Manager✈️
+          </p>
+          <p>
+            아이디어를 현실로 만드는데 진심인 사람입니다.
+            <br />
+            평소에 보고, 읽고, 겪고, 느낀 것들을 기록하고 있습니다.
+          </p>
+        </div>
+      </div>
 
-    //   <hr className="mb-4" />
-    //   <div className="my-8">
-    //     <InChat/>
-    //   </div>
+      <hr className="mb-4" />
+      <div className="my-8">
+        <InChat/>
+      </div>
 
-    //   <div className="mb-8 flex items-center justify-between">
-    //     <h3 className="text-xl font-bold tracking-tight text-center">혹시, 제가 더 궁금하신가요? 👀 </h3>
-    //     <div className="text-center">
-    //     <Link  href="/resume">
-    //         <Button className="mt-4 bg-black text-white rounded-lg px-3 py-2 transition-all ease-linear hover:bg-blue-500">궁금해요 💭</Button>
-    //       </Link>
-    //       </div>
-    //     </div>
-    // </section>
+      <div className="mb-8 flex items-center justify-between">
+        <h3 className="text-xl font-bold tracking-tight text-center">혹시, 제가 더 궁금하신가요? 👀 </h3>
+        <div className="text-center">
+        <Link  href="/resume">
+            <Button className="mt-4 bg-black text-white rounded-lg px-3 py-2 transition-all ease-linear hover:bg-blue-500">궁금해요 💭</Button>
+          </Link>
+          </div>
+        </div>
+    </section>
 
   );
 }
